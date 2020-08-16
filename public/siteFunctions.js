76096dff428b8e831d63206355d2ec25fd5ba08a
Anthony McGrath <akm@anthonykyle.co.nz>
2020-08-17 08:31:04 +1200

    Fix function call on all pages causing console error, tweak form layout

--- public/siteFunctions.js
+++ public/siteFunctions.js
@@ -1,8 +1,14 @@
-document.addEventListener('keypress', editJoke);
+function addListener(){
+  document.addEventListener('keypress', editJoke);
+}
 
 function editJoke(evt){
   if (evt.key === 'e'){
     const id = document.getElementById('jokeId').textContent;
     window.location.href = "/edit/" + id;
   }
+}
+
+if (window.location.pathname === '/'){
+  addListener();
 }
